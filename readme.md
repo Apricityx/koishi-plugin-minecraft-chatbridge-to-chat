779ad7bf3809b725147dbff08d1ee1d32e1ba400
Apricityx <apricityx@qq.com>
2024-01-15 03:25:18 +0800

    BUG修复+适配red协议

--- readme.md
+++ readme.md
@@ -13,3 +13,13 @@ Koishi: (https://koishi.chat/zh-CN/)
 Chatbridge端配置方法和原来的Gocq一样，只是把原先gocq的ws连接地址改为Koishi地址+此插件配置页面指定端口
 
 关于Chatbridge的部署可以参考这篇帖子：(https://blog.mctown.tech/2022/11/17/%E5%90%8C%E6%AD%A5%E4%B8%8D%E5%90%8C%E6%9C%8D%E5%8A%A1%E7%AB%AF%E9%97%B4%E7%9A%84%E6%B6%88%E6%81%AF-Chatbridge/)
+
+-一些注意事项：
+
+Chatbridge的ws老是连不上，请检查是否是用docker部署，如果是，记得把端口映射到宿主机上，不然会出现无法连接的情况
+
+Koishi配置项的群号需要与Chatbridge的群号一致，否则消息不会正常发送
+
+群号可以用Koishi的inspect指令查询，请填写频道 ID
+
+在修改配置后，需要重启Chatbridge的gocq端，否则会漏消息
--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,14 @@ export interface Config {
 
 export const Config: Schema<Config> = Schema.object({
   groupId: Schema.string().description('群号（需要和CB配置文件中的保持一致）：').required(),
-  adapter: Schema.union(['Red协议 对接adapter-red ','Kook','QQ频道'] as const).description('选择适配器').required(),
+  adapter: Schema.union(['Red协议(对接adapter-red)', 'Kook'] as const).description('选择适配器').required(),
   port: Schema.number().description('监听的端口').default(6700),
 })
 
 export function apply(ctx: Context, config: Config) {
   const logger = ctx.logger('Minecraft') // 创建logger用于输出Koishi日志
   const logger_log = ctx.logger('Log') // 创建logger用于输出Koishi日志
+  const logger_qq = ctx.logger('QQ') // 创建logger用于输出Koishi日志
   ctx.on('dispose', () => {
     // 在插件停用时关闭端口
     server.close()
@@ -24,18 +25,18 @@ export function apply(ctx: Context, config: Config) {
   let temp = config["adapter"];
   let adapter: string;
   switch (temp) {
-    case "red协议(对接adapter-red)":
+    case "Red协议(对接adapter-red)":
       adapter = "red";
       break;
     case "Kook":
       adapter = "kook";
       break;
     case "QQ频道":
-        adapter = "qqguild";
-        break;
+      adapter = "qqguild";
+      break;
   }
   logger_log.success("已选择平台：" + adapter)
-  const groupId :number = +config["groupId"]
+  const groupId: number = +config["groupId"]
   const broadcastTo = adapter + ':' + groupId
   const port = config["port"]
   const server = new WebSocket.Server({port: port});
@@ -47,10 +48,8 @@ export function apply(ctx: Context, config: Config) {
       str = str['params']['message']
       //将
       logger.info(`收到消息: ${str}`);
-      if (adapter === "kook") {
-        str = str.replace(/</g, "");
-        str = str.replace(/>/g, ":");
-      }
+      str = str.replace(/</g, "&lt;");
+      str = str.replace(/>/g, "&gt;");
       ctx.broadcast([broadcastTo], str)
     });
 
@@ -59,19 +58,45 @@ export function apply(ctx: Context, config: Config) {
     });
 
     ctx.on('message', (session) => {
-      let temp: number = +session.channelId
-      if (temp !== groupId) return
-      if (session.bot['user']['username'] !== session.event.message['user']['name']) {
-        let data = {
-          'post_type': 'message',
-          'message_type': 'group',
-          'group_id': groupId,
-          'anonymous': null,
-          'sender': {
-            'card': session.event.message['user']['name']
-          },
-          'raw_message': session.event.message['content'],
+      if (adapter === "kook") {
+        let temp: number = +session.channelId
+        logger_qq.info(`收到消息: ${session.event.message}`)
+        if (temp !== groupId) return
+        if (session.bot['user']['username'] !== session.event.message['user']['name']) {
+          let data = {
+            'post_type': 'message',
+            'message_type': 'group',
+            'group_id': groupId,
+            'anonymous': null,
+            'sender': {
+              'card': session.event.message['user']['name']
+            },
+            'raw_message': session.event.message['content'],
+          }
+          ws.send(JSON.stringify(data));
         }
+      }
+
+
+
+      if (adapter === "red") {
+        let temp: number = +session.channelId
+        console.log(session.channelId,'这是频道id')
+        console.log(session.event)
+        let person = session.event['user']['name']
+        let message = session.event['message']['content']
+        logger_qq.info(`收到消息: <${person}>: ${message}`)
+        if (temp !== groupId) return
+          let data = {
+            'post_type': 'message',
+            'message_type': 'group',
+            'group_id': groupId,
+            'anonymous': null,
+            'sender': {
+              'card': person
+            },
+            'raw_message': message,
+          }
         ws.send(JSON.stringify(data));
       }
     })
